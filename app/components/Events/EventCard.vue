6daad7b38d3e0b927ee9a867c79b0743fc1f4329
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-04 22:01:07 +0100

    Updated Changes

--- app/components/Events/EventCard.vue
+++ app/components/Events/EventCard.vue
@@ -85,8 +85,7 @@
             <svg class="w-4 h-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
               <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
             </svg>
-            <!-- <span class="truncate">{{ formattedTime }}</span> -->
-            <span class="truncate">12:00PM</span>
+            <span class="truncate">{{ formattedTime }}</span>
           </div>
           <div class="text-sm text-gray-600 flex items-center gap-2 mt-1">
             <svg class="w-4 h-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
