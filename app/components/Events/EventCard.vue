a20d0838418f00b8db70d7d10f9d3fbe06350ee8
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-04 18:21:00 +0100

    Updated Changes

--- app/components/Events/EventCard.vue
+++ app/components/Events/EventCard.vue
@@ -31,7 +31,7 @@
       </div>
       
       <!-- Error State -->
-      <div v-if="imageError" class="absolute inset-0 bg-gradient-to-br from-gray-100 to-gray-200 flex items-center justify-center">
+      <div v-if="imageError" class="absolute inset-0  flex items-center justify-center">
         <div class="text-center">
           <svg class="w-10 h-10 text-gray-400 mx-auto mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
@@ -75,7 +75,6 @@
         </div>
       </div>
     </div>
-    <p>{{ event.featured_image }}</p>
 
     <!-- Event Content -->
     <div class="p-6">
