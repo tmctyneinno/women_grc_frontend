52f383d9ce57842da946e27dcc9d131a1a8dfe71
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-04 18:51:01 +0100

    Updated Changes

--- app/pages/events/[slug].vue
+++ app/pages/events/[slug].vue
@@ -164,7 +164,7 @@
                   </div>
                 </div>
               </div>
-              
+              <p>{{ event.meeting_link }}</p>
               <!-- Call to Action -->
               <div class="flex flex-wrap gap-4">
                 <button 
