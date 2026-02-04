ff58d05066212807d978d1b784b66c256720cd61
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-04 13:07:13 +0100

    Updated Changes

--- app/layouts/landingLayout.vue
+++ app/layouts/landingLayout.vue
@@ -1,21 +1,4 @@
 <template>
-    <Head>
-      <link 
-        rel="stylesheet" 
-        href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
-      />
-      <!-- Script configuration -->
-      <script>
-        // Configure Tailwind to avoid conflicts with Bootstrap
-        tailwind.config = {
-          prefix: 'tw-',
-          important: '.tw-tailwind-container',
-          corePlugins: {
-            preflight: false,
-          }
-        }
-      </script>
-    </Head>
     <landing-navbar-component />
     <div class="min-vh-100 pb-5 overflow-hidden">
         <slot />
