7f129ac757d91a57636c85ff01e79f37a249db84
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-05 08:45:10 +0100

    Updated Changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: true,
   routeRules: {
     '/api/images/**': {
       // proxy: 'http://localhost:8000/storage/**' 
