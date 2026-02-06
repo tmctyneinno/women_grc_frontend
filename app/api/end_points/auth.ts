8940f622fe576dff48cb1d4c59ac10907e9c1917
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-06 23:28:46 +0100

    Updated Changes

--- app/api/end_points/auth.ts
+++ app/api/end_points/auth.ts
@@ -1,6 +1,25 @@
 import { $instance, $instanceForm } from '../instances'
 
+export default {
 
+    register(data: any) {
+        return $instance.post(`register`, JSON.stringify(data))
+    },
+
+ 
+    login(data: any) {
+        return $instance.post(`login`, JSON.stringify(data))
+    },
+
+
+    logout() {
+        return $instance.get(`logout`)
+    },
+
+    profile() {
+        return $instance.get(`profile`)
+    },
+}
 // API methods
 export default {
     register(data) {
